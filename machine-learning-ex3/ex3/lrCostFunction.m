2c27997436aaa84b16a8023003dea5a987d5de2f
dtuckersuh <suh977@gmail.com>
2020-02-12 21:22:04 -0800

    first commit

---
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -0,0 +1,71 @@
+function [J, grad] = lrCostFunction(theta, X, y, lambda)
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%regularization
+%   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Hint: The computation of the cost function and gradients can be
+%       efficiently vectorized. For example, consider the computation
+%
+%           sigmoid(X * theta)
+%
+%       Each row of the resulting matrix will contain the value of the
+%       prediction for that example. You can make use of this to vectorize
+%       the cost function and gradient computations. 
+%
+% Hint: When computing the gradient of the regularized cost function, 
+%       there're many possible vectorized solutions, but one solution
+%       looks like:
+%           grad = (unregularized gradient for logistic regression)
+%           temp = theta; 
+%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           grad = grad + YOUR_CODE_HERE (using the temp variable)
+%
+
+% Each row of h_theta is value of prediction for that training example
+h_theta = sigmoid(X * theta);
+
+% Cost (regularized)
+J = (1/m) * (((-1 * y') * log(h_theta)) - ((1-y') * log(1-h_theta))) + (lambda/(2*m)) * (sum(theta(2:end) .^ 2));
+
+% Vectorized Gradient
+%disp("Size of h_theta: "), disp(size(h_theta)); % 5 x 1
+%disp("Size of y: "), disp(size(y)); % 5 x 1
+%disp("Size of X: "), disp(size(X)); % 5 x 4
+% theta = 4 x 1
+% X' = 4 x 5
+
+grad = (1 / m) * X' * (h_theta - y)
+temp = theta;
+temp(1) = 0;
+grad = grad + temp * (lambda / m);
+
+
+
+
+
+
+
+
+
+
+
+% =============================================================
+
+grad = grad(:);
+
+end
